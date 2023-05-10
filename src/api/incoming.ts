cc1af0b14632f213bef209e4b850ad94610c9206
Margus Havik <margus.havik@devtailor.com>
2023-05-10 10:53:03 +0300

    "route configurations with path variable extraction"

--- src/api/incoming.ts
+++ src/api/incoming.ts
@@ -6,39 +6,34 @@ import { analyze } from '../modules/analyzer/analyzer.module';
 
 const getHandler: RouteHandler = async (request: FastifyRequest, reply: FastifyReply) => {
   const headers: IncomingHttpHeaders = request.headers;
-  const queryParams = request.params;
-  request.log.info('raw:', request.raw);
-  request.log.info('raw headers:', request.raw.headers);
-  request.log.info('params:', request.params);
-
-  analyze(headers, queryParams, undefined);
-
-  reply.send({ code: 200, status: 'OK' });
+  const queryParams = request.query;
+  const pathParams = request.params;
+  request.log.info('headers: %o', headers);
+  request.log.info('query params: %o', queryParams);
+  request.log.info('path params: %o', pathParams);
+  analyze(headers, pathParams, undefined);
+  reply.send({ code: 200, status: 'OK', content: { headers, pathParams, queryParams } });
 };
 
 const postHandler: RouteHandler = async (request: FastifyRequest, reply: FastifyReply) => {
   const headers: IncomingHttpHeaders = request.headers;
   const body = request.body;
-  request.log.info('raw:', request.raw);
-  request.log.info('raw headers:', request.raw.headers);
-  request.log.info('params:', request.params);
+  const pathParams = request.params;
+  const queryParams = request.query;
+  request.log.info('headers: %o', headers);
+  request.log.info('query params: %o', queryParams);
+  request.log.info('path params: %o', pathParams);
+  request.log.info('body: %o', body);
   analyze(headers, undefined, body);
-  reply.send({ code: 200, status: 'OK' });
+  reply.send({ code: 200, status: 'OK', content: { headers, pathParams, queryParams, body } });
 };
 
 const get: RouteOptions = {
   method: 'GET',
-  url: '/ruuter-incoming',
+  url: '/ruuter-incoming*',
   schema: {
-    querystring: {},
-    response: {
-      200: {
-        type: 'object',
-        properties: {
-          code: { type: 'number' },
-          status: { type: 'string' },
-        },
-      },
+    querystring: {
+      chat: { type: 'string' },
     },
   },
   handler: getHandler,
@@ -46,19 +41,7 @@ const get: RouteOptions = {
 
 const post: RouteOptions = {
   method: 'POST',
-  url: '/ruuter-incoming',
-  schema: {
-    querystring: {},
-    response: {
-      200: {
-        type: 'object',
-        properties: {
-          code: { type: 'number' },
-          status: { type: 'string' },
-        },
-      },
-    },
-  },
+  url: '/ruuter-incoming*',
   handler: postHandler,
 };
 
--- src/modules/analyzer/analyzer.module.ts
+++ src/modules/analyzer/analyzer.module.ts
@@ -1,5 +1,3 @@
 export const analyze = (headers: any, queryParams: any, body: any) => {
-  console.log('headers:', JSON.stringify(headers));
-  console.log('params:', JSON.stringify(queryParams));
-  console.log('body', JSON.stringify(body));
+
 };
