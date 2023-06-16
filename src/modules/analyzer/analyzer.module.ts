405479d542ae95c0458f25b88d0cc2499e0f6590
Margus Havik <margus.havik@devtailor.com>
2023-06-17 00:09:21 +0300

    application.yml loading as application config

--- src/modules/analyzer/analyzer.module.ts
+++ src/modules/analyzer/analyzer.module.ts
@@ -1,1 +1,5 @@
+import fastify from 'fastify';
+
+import { config } from '../../app/config';
+
 export const analyze = (path: string, method: string, headers: any, queryParams: any, body?: any) => {};
