24fec29d1920b1d1268e0ef8b6585b44ddd2efeb
Margus Havik <margus.havik@devtailor.com>
2023-06-17 10:58:48 +0300

    minor fixes and README update

--- src/modules/analyzer/analyzer.module.ts
+++ src/modules/analyzer/analyzer.module.ts
@@ -1,5 +1,1 @@
-import fastify from 'fastify';
-
-import { config } from '../../app/config';
-
 export const analyze = (path: string, method: string, headers: any, queryParams: any, body?: any) => {};
